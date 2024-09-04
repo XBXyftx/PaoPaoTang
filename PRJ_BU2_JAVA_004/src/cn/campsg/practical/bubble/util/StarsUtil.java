43bff1504cdbcb41b539c1560bc9df5d91292a5a
一方通行 <145579105+XBXyftx@users.noreply.github.com>
2024-09-04 09:14:50 +0800

    a

---
+++ PRJ_BU2_JAVA_004/src/cn/campsg/practical/bubble/util/StarsUtil.java
@@ -0,0 +1,59 @@
+package cn.campsg.practical.bubble.util;
+
+import cn.campsg.practical.bubble.entity.Position;
+import cn.campsg.practical.bubble.entity.Star;
+import cn.campsg.practical.bubble.entity.Star.StarType;
+
+/**
+ * 泡泡糖/待移动泡泡糖实体类工具类，提供以下功能：<br>
+ * <ul>
+ * <li>1. 排序指定列表中的泡泡糖-按列升序排序，列相同按行升序排序（冒泡排序算法）。</li>
+ * <li>2. 对指定列表中的泡泡糖按行分组。</li>
+ * <li>3. 克隆一个泡泡糖对象</li>
+ * </ul>
+ * 
+ * @see cn.campsg.practical.bubble.entity.Star
+ * @see cn.campsg.practical.bubble.entity.MovedStar
+ * 
+ * 
+ * @author Frank.Chen
+ * @version 1.5
+ *
+ */
+public class StarsUtil {
+	/**
+	 * 克隆一个新的泡泡糖。
+	 * 
+	 * @param star
+	 *            待克隆的泡泡糖
+	 * @return 新的泡泡糖（独立内存地址的泡泡糖）。
+	 */
+	/******************** PRJ-BU2-JAVA-004 Task1 ********************/
+	public static Star clone(Star star){
+		return new Star(new Position(star.getPosition().getRow(),star.getPosition().getColumn()),star.getType());
+	}
+	public static void main (String[] args){
+		Star star = new Star(new Position(5,5),StarType.RED);
+		System.out.println(star);
+		Star star2 = clone(star);
+		System.out.println(star2);
+		System.out.println(equals(star,star2));
+		System.out.println(equals(star.getPosition(),star2.getPosition()));
+	}
+	public static boolean equals(Object star1,Object star2 ) {
+        if (star1 == star2) { // 如果是同一个对象，直接返回true
+            return true;
+        }else{
+        	return false;
+        }
+     }
+	
+	
+	
+	
+	
+	
+	
+	
+	/**************************************************************/
+}
